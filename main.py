4a3cb400c1db517f2194c727c421239983e62287
lawosn <132624579+lawosn@users.noreply.github.com>
2023-11-03 18:34:11 +0000

    added another layer to the story, with more options

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ import time
 # 1. Investigate
 # 2. Act like nothing happened
 
+# If you choose to investigate:
+# You break down the stall door, and there a creature awaits you.
+# Without much delay or talk, the creature jumps at you and kills you.
+
+# If you choose to act like nothing happened:
+# You go about your routine as usual, albeit in an unsual location.
+# When you walk out of the bathroom, you notice the rest of your stuff is gone.
+# You assume it was just one of your friends that arrived super early
+# and brought your stuff to class for you.
+# You start heading to your first class.
+# There is a creepy sound following you as you go through the hallway.
+# What do you do?
+# 1. Keep going
+# 2. Run home
+
 def delay():
   time.sleep(8)
