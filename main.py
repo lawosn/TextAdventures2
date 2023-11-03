7796d1163e0d5cde47aff56cf473237711e32580
lawosn <132624579+lawosn@users.noreply.github.com>
2023-11-03 18:25:19 +0000

    added first section of story + options, added delay function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import time
+
+# Haunted School Story:
+# You wake up to the sound of your wonderful iPhone alarm.
+# You look around, and notice you never went home after school.
+# "Huh, weird" you say to yourself as you walk to the bathroom to get ready.
+# While in the bathroom, you see blood flowing out from under a stall.
+# What do you do?
+# 1. Investigate
+# 2. Act like nothing happened
+
+def delay():
+  time.sleep(8)
